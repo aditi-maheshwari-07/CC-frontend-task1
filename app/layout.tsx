12ce61ab234b85a64d581be102c09d263ed4fb50
Aditi Maheshwari <aditimaheshwari193@gmail.com>
2026-06-23 12:15:30 +0530

    updated dashboard

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
-        <head> <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <head><meta name="viewport" content="width=device-width, initial-scale=1.0" />
             <title></title>
         </head>
         <body>{children}</body>
